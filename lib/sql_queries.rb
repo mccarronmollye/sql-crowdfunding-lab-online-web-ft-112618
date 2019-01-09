ebaf028a934f96fb400df2c849f6695c5cd9d94c
Molly McCarron <mccarronmollye@gmail.com>
2019-01-09 18:50:18 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT title FROM projects
  JOIN pledges ON projects.id = pledges.project_id
- ORDER by projects.title"
+ GROUP by projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
